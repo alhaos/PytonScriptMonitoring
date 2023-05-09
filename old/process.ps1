95776a48ca8c7c1b0e357451667d3249038cd995
alhaos <52729006+alhaos@users.noreply.github.com>
2023-05-09 21:20:15 +0500

    New link from NONE. Waiting...

--- old/process.ps1
+++ old/process.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
     A longer description of the function, its purpose, common use cases, etc.
 .NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
+    Information or caveats about the functpfuhЗion e.g. 'This function is not supported in Linux'
 .LINK
     Specify a URI to a help page, this will show when Get-Help -Online is used.
 .EXAMPLE
